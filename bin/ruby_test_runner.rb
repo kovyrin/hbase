db0627e99b7ec32ec9c615d335b394103974bd45
Alexey Kovyrin <alexey@kovyrin.net>
2010-03-06 21:06:39 -0500

    Cleaned up the test suite running scripts + added helper + added some tests for Shell::Formatter

--- bin/ruby_test_runner.rb
+++ bin/ruby_test_runner.rb
@@ -1,6 +1,9 @@
-#!/usr/bin/env ruby
+include Java
+require "lib/ruby/test/test_helper"
+
+# Set logging level to avoid verboseness
+org.apache.log4j.Logger.getLogger("org.apache.zookeeper").setLevel(org.apache.log4j.Level::ERROR)
+org.apache.log4j.Logger.getLogger("org.apache.hadoop.hbase").setLevel(org.apache.log4j.Level::ERROR)
 
 # Load the test files from the command line.
-
-ARGV.each { |f| load f unless f =~ /^-/  }
-
+ARGV.each { |f| load(f) unless f =~ /^-/  }
