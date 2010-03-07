b615f79d33bb80bc47f9db8b388f834734ba85a6
Alexey Kovyrin <alexey@kovyrin.net>
2010-03-06 23:32:10 -0500

    Added an empty hbase::admin test suite + started covering hbase::table with tests

--- lib/ruby/test/hbase/table_test.rb
+++ lib/ruby/test/hbase/table_test.rb
@@ -5,10 +5,23 @@ module Hbase
   class TableTest < Test::Unit::TestCase
     def setup
       @formatter = Shell::Formatter::Console.new(:format_width => 110)
+      @hbase = ::Hbase::Hbase.new(@formatter)
     end
 
-    def test_pass
-      assert(true)
+    def table(table)
+      @hbase.table(table)
+    end
+
+    define_test "Hbase::Table constructor should fail for non-existent tables" do
+      assert_raise(NativeException) do
+        table('non-existent-table-name')
+      end
+    end
+
+    define_test "Hbase::Table constructor should not fail for existent tables" do
+      assert_nothing_raised do
+        table('.META.')
+      end
     end
   end
 end
