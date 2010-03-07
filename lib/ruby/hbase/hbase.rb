f1b8616b20f7a6f30c3a0d0a967b69d75add2daf
Alexey Kovyrin <alexey@kovyrin.net>
2010-03-07 02:08:40 -0500

    Move formatter instance to the shell, preparing to moving all the formatting code to command classes

--- lib/ruby/hbase/hbase.rb
+++ lib/ruby/hbase/hbase.rb
@@ -3,11 +3,10 @@ require 'hbase/table'
 
 module Hbase
   class Hbase
-    attr_accessor :configuration, :formatter
+    attr_accessor :configuration
 
-    def initialize(formatter)
-      # Save formatter and create configuration
-      self.formatter = formatter
+    def initialize
+      # Create configuration
       self.configuration = HBaseConfiguration.create
 
       # Turn off retries in hbase and ipc.  Human doesn't want to wait on N retries.
@@ -15,12 +14,12 @@ module Hbase
       configuration.setInt("ipc.client.connect.max.retries", 3)
     end
 
-    def admin
-      @admin ||= ::Hbase::Admin.new(configuration, formatter)
+    def admin(formatter)
+      ::Hbase::Admin.new(configuration, formatter)
     end
 
     # Create new one each time
-    def table(table)
+    def table(table, formatter)
       ::Hbase::Table.new(configuration, table, formatter)
     end
   end
