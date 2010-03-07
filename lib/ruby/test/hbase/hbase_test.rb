a4bf24592374dc1163fa8fe42f6824ca336d9b1f
Alexey Kovyrin <alexey@kovyrin.net>
2010-03-06 21:49:29 -0500

    Cleanup Hbase::Admin + add tests for Hbase::Hbase

--- lib/ruby/test/hbase/hbase_test.rb
+++ lib/ruby/test/hbase/hbase_test.rb
@@ -8,8 +8,29 @@ module Hbase
       @formatter = Shell::Formatter::Console.new(:format_width => 110)
     end
 
-    define_test "should pass" do
-      assert(true)
+    define_test "Hbase::Hbase constructor should initialize hbase configuration object" do
+      hbase = ::Hbase::Hbase.new(@formatter)
+      assert_kind_of(org.apache.hadoop.conf.Configuration, hbase.configuration)
+    end
+
+    define_test "Hbase::Hbase#admin should create a new admin object when called the first time" do
+      hbase = ::Hbase::Hbase.new(@formatter)
+      assert_kind_of(::Hbase::Admin, hbase.admin)
+    end
+
+    define_test "Hbase::Hbase#admin should use cahced admin object when possible" do
+      hbase = ::Hbase::Hbase.new(@formatter)
+      assert_same(hbase.admin, hbase.admin)
+    end
+
+    define_test "Hbase::Hbase#table should create a new table object when called the first time" do
+      hbase = ::Hbase::Hbase.new(@formatter)
+      assert_kind_of(::Hbase::Table, hbase.table('.META.'))
+    end
+
+    define_test "Hbase::Hbase#table should create a new table object every call" do
+      hbase = ::Hbase::Hbase.new(@formatter)
+      assert_not_same(hbase.table('.META.'), hbase.table('.META.'))
     end
   end
 end
