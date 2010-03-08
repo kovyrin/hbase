e78e6f0c5bf54ab8fb9054316b06cc2a055ea00e
Alexey Kovyrin <alexey@kovyrin.net>
2010-03-08 17:44:15 -0500

    zk_dump and create covered

--- lib/ruby/test/hbase/admin_test.rb
+++ lib/ruby/test/hbase/admin_test.rb
@@ -43,6 +43,9 @@ module Hbase
       # Create test table if it does not exist
       @test_name = "hbase_shell_tests_table"
       create_test_table(@test_name)
+
+      # Create table test table name
+      @create_test_name = 'hbase_create_table_test_table'
     end
 
     define_test "list should return a list of tables" do
@@ -96,6 +99,54 @@ module Hbase
       admin.disable(@test_name)
       admin.drop(@test_name)
       assert(!admin.exists?(@test_name))
+    end
+
+    #-------------------------------------------------------------------------------
+
+    define_test "zk_dump should work" do
+      assert_not_nil(admin.zk_dump)
+    end
+
+    #-------------------------------------------------------------------------------
+
+    define_test "create should fail with non-string table names" do
+      assert_raise(ArgumentError) do
+        admin.create(123, 'xxx')
+      end
+    end
+
+    define_test "create should fail with non-string/non-hash column args" do
+      assert_raise(ArgumentError) do
+        admin.create(@create_test_name, 123)
+      end
+    end
+
+    define_test "create should fail without columns" do
+      if admin.exists?(@create_test_name)
+        admin.disable(@create_test_name)
+        admin.drop(@create_test_name)
+      end
+      assert_raise(ArgumentError) do
+        admin.create(@create_test_name)
+      end
+    end
+
+    define_test "should work with string column args" do
+      if admin.exists?(@create_test_name)
+        admin.disable(@create_test_name)
+        admin.drop(@create_test_name)
+      end
+      admin.create(@create_test_name, 'a', 'b')
+      assert_equal(['a:', 'b:'], table(@create_test_name).get_all_columns.sort)
+     end
+
+    define_test "should work with hash column args" do
+      if admin.exists?(@create_test_name)
+        admin.disable(@create_test_name)
+        admin.drop(@create_test_name)
+      end
+      admin.create(@create_test_name, { NAME => 'a'}, { NAME => 'b'})
+      assert_equal(['a:', 'b:'], table(@create_test_name).get_all_columns.sort)
     end
   end
 end
